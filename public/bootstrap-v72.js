3e1f0bc45c67ce08c6e18d1c6aa13281f9d8c5fd
HardandCorez <officialcrazyagent@gmail.com>
2026-09-07 00:38:52 +1000

    Load V9 stylesheet and suite

--- public/bootstrap-v72.js
+++ public/bootstrap-v72.js
@@ -1,3 +1,8 @@
+const v9css = document.createElement('link');
+v9css.rel = 'stylesheet';
+v9css.href = '/v9.css';
+document.head.appendChild(v9css);
+
 await import('./v9-suite.js');
 await import('./visual-coach-v72.js');
 await import('./app.js');
